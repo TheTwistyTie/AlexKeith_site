b05c46f0218e7b2743472aa4d0e2f54e79b80cc9
Alexander Keith <alexander.n.keith@gmail.com>
2022-02-12 15:17:32 -0500

    f

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'build')))
 app.get('/*', function(req,res){
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
   });
 const server = http.createServer(app);
 const port = 9000;
