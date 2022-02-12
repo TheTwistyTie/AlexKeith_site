eb7484695e5ae1d13f9a57ac05c7681a86937d4e
Alexander Keith <alexander.n.keith@gmail.com>
2022-02-12 14:40:29 -0500

    testing still

--- server.js
+++ server.js
@@ -2,14 +2,11 @@ const http = require('http');
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.json());
-app.use(express.static("express"));
-// default URL for website
-app.use('/alexkeithsite/', function(req,res){
-    res.sendFile(path.join(__dirname+'/express/index.html'));
-    //__dirname : It will resolve to your project folder.
+app.use(express.static(path.join(__dirname, 'build')))
+app.get('/*', function(req,res){
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 const server = http.createServer(app);
-const port = 3000;
+const port = 9000;
 server.listen(port);
 console.debug('Server listening on port ' + port);
